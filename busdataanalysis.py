4068c26e588dfbf9af3dd9800508752a813c494e
sralloza <sralloza@gmail.com>
2018-12-12 22:28:13 +0100

    Minor changes

--- busdataanalysis.py
+++ busdataanalysis.py
@@ -16,42 +16,45 @@ class ExtraDato(Dato):
         hora, minuto = [int(x) for x in self.ta.split(':')]
         self.dt = (datetime.combine(date.today(), time(hora, minuto)) + timedelta(minutes=self.tr))  # .time()
 
+def analizar():
+    with open('data.json', 'rt', encoding='utf-8') as fh:
+        datos = json.load(fh)
 
-with open('data.json', 'rt', encoding='utf-8') as fh:
-    datos = json.load(fh)
+    # noinspection PyArgumentList
+    datos = [ExtraDato(**x) for x in datos]
 
-# noinspection PyArgumentList
-datos = [ExtraDato(**x) for x in datos]
+    datos.sort(key=lambda x: x.dt)
 
-datos.sort(key=lambda x: x.dt)
+    medidas = DataFrame([astuple(x) for x in datos], columns=['linea', 'ta', 'tr', 'id_parada', 'dtm'])
+    # medias = []
+    # temp = []
+    # for i in range(len(datos)):
+    #     print(datos[i])
+    #     try:
+    #         if abs(datos[i].dt - datos[i + 1].dt) <= timedelta(minutes=5):
+    #             temp.append(datos[i])
+    #         else:
+    #             datetimes = [x.dt for x in temp]
+    #             medias.append(mean([x.dt - min(datetimes) for x in temp]) + min(datetimes))
+    #             temp = []
+    #     except IndexError:
+    #         datetimes = [x.dt for x in temp]
+    #         medias.append(mean([x.dt - min(datetimes) for x in temp]) + min(datetimes))
+    #         temp = []
+    #
+    # medias = [x.time() for x in medias]
+    # for i in range(len(medias)):
+    #     medias[i] = medias[i].replace(microsecond=0)
+    #
+    # plt.scatter(range(len(medias)), medias)
+    # plt.show()
 
-medidas = DataFrame([astuple(x) for x in datos], columns=['linea', 'ta', 'tr', 'id_parada', 'dtm'])
-# medias = []
-# temp = []
-# for i in range(len(datos)):
-#     print(datos[i])
-#     try:
-#         if abs(datos[i].dt - datos[i + 1].dt) <= timedelta(minutes=5):
-#             temp.append(datos[i])
-#         else:
-#             datetimes = [x.dt for x in temp]
-#             medias.append(mean([x.dt - min(datetimes) for x in temp]) + min(datetimes))
-#             temp = []
-#     except IndexError:
-#         datetimes = [x.dt for x in temp]
-#         medias.append(mean([x.dt - min(datetimes) for x in temp]) + min(datetimes))
-#         temp = []
-#
-# medias = [x.time() for x in medias]
-# for i in range(len(medias)):
-#     medias[i] = medias[i].replace(microsecond=0)
-#
-# plt.scatter(range(len(medias)), medias)
-# plt.show()
+    medidas.set_index('ta')
+    medidas.loc[:, 'dtm'] = medidas.apply(lambda x: x.dtm.time(), axis=1)
+    print(medidas)
 
-medidas.set_index('ta')
-medidas.loc[:, 'dtm'] = medidas.apply(lambda x: x.dtm.time(), axis=1)
-print(medidas)
+    medidas.plot()
+    plt.show()
 
-medidas.plot()
-plt.show()
+if __name__ == '__main__':
+    analizar()
